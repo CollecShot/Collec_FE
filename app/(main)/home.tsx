c47adde7eb5b753c139787da6b730eaa9064e12d
Minji Kim <minjikim2000@hotmail.com>
2025-03-25 21:10:44 +0900

    ✨ feat: add home route #3

---
+++ app/(main)/home.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>여기가 홈!</Text>
+    </View>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,35 @@
-import { Text, View } from "react-native";
+import { ROUTES } from "@/src/constants/routes";
+import { useRouter } from "expo-router";
+import { useEffect } from "react";
+import { ActivityIndicator, View } from "react-native";
 
 export default function Index() {
+  const router = useRouter();
+
+  useEffect(() => {
+    async function checkFirstLaunch() {
+      const isFirstLogin = await isUserFirstLogin();
+
+      if (isFirstLogin) {
+        // 온보딩(랜딩) 페이지로 이동
+        //router.replace("(onboarding)/step1");
+      } else {
+        // 이미 온보딩을 마친 사용자 → 메인으로
+        router.replace(ROUTES.MAIN_HOME);
+      }
+    }
+    checkFirstLaunch();
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <ActivityIndicator size="large" />
     </View>
   );
+}
+
+// TODO: AsyncStorage or API 사용용
+async function isUserFirstLogin() {
+  // TODO: 실제 로직
+  return false;
 }
