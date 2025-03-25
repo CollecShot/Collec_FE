db5e28b37cc4cf7890a06751914cabb56e71bcf1
Minji Kim <minjikim2000@hotmail.com>
2025-03-26 00:12:16 +0900

    ✨ feat: add constants for categories #3

---
+++ src/constants/categories.ts
@@ -0,0 +1,12 @@
+export const CATEGORY_TITLES: { [key: string]: string } = {
+  shopping: "쇼핑 & 구매",
+  document: "문서 & 정보",
+  reservation: "거래 & 예약",
+  location: "장소",
+  coupon: "쿠폰",
+  chat: "대화기록",
+  music: "노래",
+  animal: "동물",
+  person: "인물",
+  etc: "기타",
+};
