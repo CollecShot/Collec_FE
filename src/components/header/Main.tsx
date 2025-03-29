d3e696346a9dc7e0c2c4ecb6fef2442af11efc94
Minji Kim <minjikim2000@hotmail.com>
2025-03-30 06:06:38 +0900

    ✨ feat: apply headerContainer #3

--- src/components/header/Main.tsx
+++ src/components/header/Main.tsx
@@ -3,11 +3,11 @@ import SearchIcon from "@assets/icons/search.svg";
 import TrashIcon from "@assets/icons/trash.svg";
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";
 import { TouchableOpacity } from "react-native";
-import styled from "styled-components/native";
+import { HeaderContainer } from "../_common/styled";
 
 export default function MainHeader({ navigation, back }: NativeStackHeaderProps) {
   return (
-    <Container>
+    <HeaderContainer>
       <TouchableOpacity onPress={() => console.log("Trash icon pressed")}>
         <TrashIcon />
       </TouchableOpacity>
@@ -15,15 +15,6 @@ export default function MainHeader({ navigation, back }: NativeStackHeaderProps)
       <TouchableOpacity onPress={() => console.log("Search icon pressed")}>
         <SearchIcon />
       </TouchableOpacity>
-    </Container>
+    </HeaderContainer>
   );
 }
-
-const Container = styled.View`
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  padding: 19px 18px;
-  height: 56px;
-  background-color: ${({ theme }) => theme.colors.white[100]};
-`;
