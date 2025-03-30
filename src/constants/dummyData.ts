388798c58c79b53609dc70acbba3a2879ab3fef0
Minji Kim <minjikim2000@hotmail.com>
2025-03-31 01:03:59 +0900

    ♻️ refactor: divide dummy data for readability #3

---
+++ src/constants/dummyData.ts
@@ -0,0 +1,65 @@
+import { CATEGORY_TITLES } from "./categories";
+
+// TODO: API 연결
+export const dummyCategories = [
+  {
+    id: "shopping",
+    title: CATEGORY_TITLES.shopping,
+    imageUri: "https://i.ibb.co/YTjmxwnw/image-2.png",
+    count: 10,
+  },
+  {
+    id: "document",
+    title: CATEGORY_TITLES.document,
+    imageUri: "https://i.ibb.co/zTSWVgz9/image-3.png",
+    count: 7,
+  },
+  {
+    id: "deal",
+    title: CATEGORY_TITLES.reservation,
+    imageUri: "https://i.ibb.co/xtjJX6XG/image-4.png",
+    count: 12,
+  },
+  {
+    id: "location",
+    title: CATEGORY_TITLES.location,
+    imageUri: "https://i.ibb.co/KZ7JLtb/image-5.png",
+    count: 4,
+  },
+  {
+    id: "coupon",
+    title: CATEGORY_TITLES.coupon,
+    imageUri: "https://i.ibb.co/5hqtrdPz/image-6.png",
+    count: 15,
+  },
+  {
+    id: "chat",
+    title: CATEGORY_TITLES.chat,
+    imageUri: "https://i.ibb.co/wNsJDdHJ/image-7.png",
+    count: 3,
+  },
+  {
+    id: "music",
+    title: CATEGORY_TITLES.music,
+    imageUri: "https://i.ibb.co/jvdbCN80/image-8.png",
+    count: 8,
+  },
+  {
+    id: "animal",
+    title: CATEGORY_TITLES.animal,
+    imageUri: "https://i.ibb.co/JwJ0WD7C/Rectangle-6401.png",
+    count: 6,
+  },
+  {
+    id: "person",
+    title: CATEGORY_TITLES.person,
+    imageUri: "https://i.ibb.co/CpLg0RBq/image-9.png",
+    count: 9,
+  },
+  {
+    id: "etc",
+    title: CATEGORY_TITLES.etc,
+    imageUri: "https://i.ibb.co/PsFL8npx/image-10.png",
+    count: 2,
+  },
+];
