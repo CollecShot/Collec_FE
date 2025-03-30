cf427485f0556d8acb06eb43df411ba496712396
Minji Kim <minjikim2000@hotmail.com>
2025-03-31 01:04:35 +0900

    ✨ feat: add current category context #3

--- app/(main)/home.tsx
+++ app/(main)/home.tsx
@@ -1,10 +1,22 @@
-import Grid from "@/src/components/category/Grid";
+import { useCurrentFolder } from "@/src/contexts/CurrentFolderContext";
+import Grid from "@components/category/Grid";
+import { dummyCategories } from "@constants/dummyData";
+import { ROUTES } from "@constants/routes";
+import { router } from "expo-router";
 import styled from "styled-components/native";
 
 export default function HomeScreen() {
+  const { setCurrentFolderId } = useCurrentFolder();
+
+  const handlePressItem = (category: { id: string }) => {
+    // 선택된 카테고리 id를 Context에 저장, params 삭제 예정
+    setCurrentFolderId(category.id);
+    router.push({ pathname: ROUTES.GALLERY, params: { categoryId: category.id } });
+  };
+
   return (
     <Container>
-      <Grid />
+      <Grid data={dummyCategories} onPressItem={handlePressItem} />
     </Container>
   );
 }
