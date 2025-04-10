6fe473d441a59346427ff9cdc74b06c51103bbb3
Minji Kim <minjikim2000@hotmail.com>
2025-04-11 01:36:58 +0900

    ♻️ refactor: add index file for icons #3

---
+++ assets/icons/_index.ts
@@ -0,0 +1,19 @@
+import BackIcon from "./Back.svg";
+import InfoIcon from "./info.svg";
+import LogoIcon from "./logo.svg";
+import MenuIcon from "./menu.svg";
+import MoveFileIcon from "./moveFile.svg";
+import SearchIcon from "./search.svg";
+import TrashIcon from "./trash.svg";
+import TrashGreyIcon from "./trashGrey.svg";
+
+export {
+  BackIcon,
+  InfoIcon,
+  LogoIcon,
+  MenuIcon,
+  MoveFileIcon,
+  SearchIcon,
+  TrashGreyIcon,
+  TrashIcon,
+};
