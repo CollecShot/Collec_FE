ab52789713d3f79186504c6ab2b85928195eb322
Minji Kim <minjikim2000@hotmail.com>
2025-04-13 06:23:59 +0900

    💄 UI: add home webp image #3

--- app/(main)/home.tsx
+++ app/(main)/home.tsx
@@ -3,7 +3,10 @@ import Grid from "@components/category/Grid";
 import { dummyCategories } from "@constants/dummyData";
 import { ROUTES } from "@constants/routes";
 import { router } from "expo-router";
+import { Dimensions, Image } from "react-native";
 import styled from "styled-components/native";
+
+const windowHeight = Dimensions.get("window").height; // 기기 화면 높이 가져오기
 
 export default function HomeScreen() {
   const { setCurrentFolderId } = useCurrentFolder();
@@ -15,12 +18,35 @@ export default function HomeScreen() {
   };
 
   return (
-    <Container>
-      <Grid data={dummyCategories} onPressItem={handlePressItem} />
-    </Container>
+    <ScreenContainer>
+      <GridContainer>
+        <Grid data={dummyCategories} onPressItem={handlePressItem} />
+      </GridContainer>
+      <ImageContainer>
+        <WebPImage source={require("@assets/images/home/home.webp")} resizeMode="contain" />
+      </ImageContainer>
+    </ScreenContainer>
   );
 }
 
-const Container = styled.View`
+const ScreenContainer = styled.View`
+  flex: 1;
   background-color: ${({ theme }) => theme.colors.white[100]};
+`;
+
+const GridContainer = styled.View`
+  flex: 1;
+  z-index: 100;
+`;
+
+const ImageContainer = styled.View`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  height: ${windowHeight * 0.8}px;
+`;
+
+const WebPImage = styled(Image)`
+  width: 100%;
 `;
