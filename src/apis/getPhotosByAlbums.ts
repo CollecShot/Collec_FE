8ec973c9d92e71eb70d1581fdf6a3b1e8f505cdc
Minji Kim <minjikim2000@hotmail.com>
2025-05-08 22:55:53 +0900

    🧪 tests: add console log #3

--- src/apis/getPhotosByAlbums.ts
+++ src/apis/getPhotosByAlbums.ts
@@ -9,6 +9,6 @@ export const fetchPhotosByAlbum = async (albumId: number): Promise<Photo[]> => {
   const { data } = await api.get<Photo[]>("/photo/albums", {
     params: { albumId },
   });
-  console.log(data);
+  console.log("앨범 별 사진 가져오기", data);
   return data;
 };
