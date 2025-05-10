262256c225bbe429b78bd03f6a5b15ec78cdd800
Minji Kim <minjikim2000@hotmail.com>
2025-05-11 05:03:03 +0900

    ✨ feat: modify to sort photos by descending photoIds #3

--- src/apis/hooks/usePhotosByAlbums.ts
+++ src/apis/hooks/usePhotosByAlbums.ts
@@ -5,4 +5,7 @@ export const usePhotosByAlbums = (albumId: number) =>
   useQuery<Photo[], Error>({
     queryKey: ["photosByAlbum", albumId],
     queryFn: () => fetchPhotosByAlbum(albumId),
+    select: (data) =>
+      // 복사 후 내림차순 정렬
+      data.slice().sort((a, b) => b.photoId - a.photoId),
   });
