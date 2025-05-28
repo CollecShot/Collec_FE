a3ac49bb50ead2f01b94318042d70b361c7064b1
Minji Kim <minjikim2000@hotmail.com>
2025-05-29 00:52:03 +0900

    ✏️ typo: change file name #4

--- assets/icons/_index.ts
+++ assets/icons/_index.ts
@@ -2,7 +2,7 @@ import BackIcon from "./Back.svg";
 import CloseIcon from "./circle_close.svg";
 import InfoIcon from "./info.svg";
 import LogoIcon from "./logo.svg";
-import MenuIcon from "./menu.svg";
+import MenuIcon from "./Menu.svg";
 import MoveFileIcon from "./moveFile.svg";
 import SearchIcon from "./search.svg";
 import TrashIcon from "./trash.svg";
